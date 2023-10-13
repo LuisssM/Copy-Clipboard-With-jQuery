b4938972b5eaefd41544e7e92a6f4b0fdd50d096
LuisssM <luispsmiranda.96@gmail.com>
2023-10-13 12:08:31 +0100

    finalv

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 $(document).ready(function() {
-    //adding and removing span
+    //adding span
     function add(){
         $(".copied").addClass("bounce-effect")
     }
-
+//removing span
     function remove(){
         $(".copied").removeClass("bounce-effect")
     }
     //call func and copy the text
 
     $(".copy-btn").click(function(){
-        $("#textField").select();
-        document.execCommand("copy")
-        add();
-        setTimeout(remove, 800);
+        $("#textField").select(); //selects all text
+        document.execCommand("copy") //command that copies the text
+        add(); //span appears
+        setTimeout(remove, 800); // span goes away after 800ms
     })
 });
